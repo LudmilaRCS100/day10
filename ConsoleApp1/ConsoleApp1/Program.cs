999fda7f56262b042fc3c8717174dd649842c83c
Ludmila <luvi_lv@inbox.lv>
2018-05-18 20:08:05 +0300

    10.2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,46 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
+
         {
+            
+            //Funkcija();//
+            
+            int skaitlis1 = Convert.ToInt32(Console.ReadLine());
+            int skaitlis2 = Convert.ToInt32(Console.ReadLine());
+            int results = skaitlis1 + skaitlis2;
+            Console.WriteLine("Sanaca " + results + ".");
+            Console.ReadLine();
+
+
         }
+        /*static void Funkcija()
+        {
+            int a,b,c,d;
+
+            a = 2;
+            b = 3;
+            c = 5;
+
+
+            d = a + b + c;
+
+            Console.WriteLine(d);
+                      
+            Console.ReadLine();
+
+
+        }*/
+
+
+
+
+          
+        
+
+
     }
+
+
+    
 }
