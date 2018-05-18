4213f9553cb5d715e09a9dcf21c3d6c8067075b2
Ludmila <luvi_lv@inbox.lv>
2018-05-18 21:23:52 +0300

    10.4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
 
         {
+
             int n = 1;
             while (n++ < 6)
             {
@@ -91,4 +92,181 @@ namespace ConsoleApp1
 
 
     
+}*/
+using System;
+
+
+
+namespace Pd_2
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int choice = -1;
+
+            AdvancedMath adv = new AdvancedMath();
+
+
+
+
+
+            while (choice != 0)
+
+            {
+
+                Console.WriteLine("1- saskaitit");
+
+                Console.WriteLine("2- atnemt");
+
+                Console.WriteLine("3- reizinat");
+
+                Console.WriteLine("4- dalit");
+
+
+
+                Console.WriteLine("0- Iziet");
+
+
+
+                string izvele = Console.ReadLine();
+
+                choice = Convert.ToInt16(izvele);
+
+
+
+                switch (choice)
+
+                {
+
+                    case 1:
+
+                        Saskaitit();
+
+                        break;
+
+                    case 2:
+
+                        Atnemt();
+
+                        break;
+
+                    case 3:
+
+                        adv.Reizinat();
+
+                        break;
+
+                    case 4:
+
+                        adv.Dalit();
+
+                        break;
+
+                    case 0:
+
+                        choice = 0;
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Izveleties to, ko piedava!");
+
+                        break;
+
+                }
+
+
+
+
+
+
+
+            }
+
+        }
+
+
+
+        /*Funkcija ar parametriem sk1, sk2, kuriem izsaucot funkciju mes dosim vertibu
+
+         * */
+
+        static void Saskaitit()
+
+        {
+
+            double sk1, sk2;
+
+            string virkne1, virkne2;
+
+
+
+            Console.WriteLine("Ievadiet skaitli 1");
+
+            virkne1 = Console.ReadLine();
+
+            sk1 = Convert.ToInt16(virkne1);
+
+
+
+            Console.WriteLine("Ievadiet skaitli 2");
+
+            virkne2 = Console.ReadLine();
+
+            sk2 = Convert.ToInt16(virkne2);
+
+
+
+            Console.Write("Rezultats = ");
+
+            Console.WriteLine(sk1 + sk2);//izvada skaitlu summu
+
+        }
+
+
+
+        /*Funkcija ar parametriem sk1, sk2, kuriem izsaucot funkciju mes dosim vertibu
+
+        * */
+
+        static void Atnemt()
+
+        {
+
+            double sk1, sk2;
+
+            string virkne1, virkne2;
+
+
+
+            Console.WriteLine("Ievadiet skaitli 1");
+
+            virkne1 = Console.ReadLine();
+
+            sk1 = Convert.ToDouble(virkne1);
+
+
+
+            Console.WriteLine("Ievadiet skaitli 2");
+
+            virkne2 = Console.ReadLine();
+
+            sk2 = Convert.ToDouble(virkne2);
+
+            Console.Write("Rezultats = ");
+
+            Console.WriteLine(sk1 - sk2); //izvada skaitlu starpibu
+
+        }
+
+    }
+
 }
--- ConsoleApp1/ConsoleApp1/Simboluapstrade.cs
+++ ConsoleApp1/ConsoleApp1/Simboluapstrade.cs
@@ -1,17 +1,81 @@
 ﻿using System;
+
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+
+namespace Pd_2
+
 {
-    class Simboluapstrade
-        
-    {
-       
-            
 
+    class AdvancedMath
+
+    {
+
+        public void Reizinat()
+
+        {
+
+            double sk1, sk2;
+
+            string virkne1, virkne2;
+
+
+
+            Console.WriteLine("Ievadiet skaitli 1");
+
+            virkne1 = Console.ReadLine();
+
+            sk1 = Convert.ToDouble(virkne1);
+
+
+
+            Console.WriteLine("Ievadiet skaitli 2");
+
+            virkne2 = Console.ReadLine();
+
+            sk2 = Convert.ToDouble(virkne2);
+
+            Console.Write("Rezultats = ");
+
+            Console.WriteLine(sk1 * sk2); //izvada skaitlu starpibu
+
+        }
+
+
+
+        public void Dalit()
+
+        {
+
+            double sk1, sk2;
+
+            string virkne1, virkne2;
+
+
+
+            Console.WriteLine("Ievadiet skaitli 1");
+
+            virkne1 = Console.ReadLine();
+
+            sk1 = Convert.ToDouble(virkne1);
+
+
+
+            Console.WriteLine("Ievadiet skaitli 2");
+
+            virkne2 = Console.ReadLine();
+
+            sk2 = Convert.ToDouble(virkne2);
+
+            Console.Write("Rezultats = ");
+
+            Console.WriteLine(sk1 / sk2); //izvada skaitlu starpibu
+
+        }
 
     }
+
 }
