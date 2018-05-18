6e941fee54f26ffbc55b86a046ac30632c1e1037
Ludmila <luvi_lv@inbox.lv>
2018-05-18 20:16:16 +0300

    10.3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,9 +11,47 @@ namespace ConsoleApp1
         static void Main(string[] args)
 
         {
-            
+            int n = 1;
+            while (n++ < 6)
+            {
+                Console.WriteLine("Current value of n is {0}", n);
+            }
+
+            int caseSwitch = 1;
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+            switch (caseSwitch)
+            {
+                // The following switch section causes an error.  
+                case 1:
+                    Console.WriteLine("Case 1...");
+                    break;
+                case 2:
+                case 3:
+                    Console.WriteLine("... and/or Case 2");
+                    break;
+                case 4:
+                    while (true)
+                        Console.WriteLine("Endless looping. . . .");
+                default:
+                    Console.WriteLine("Default value...");
+                    break;
+            }
+
+
             //Funkcija();//
-            
+
             int skaitlis1 = Convert.ToInt32(Console.ReadLine());
             int skaitlis2 = Convert.ToInt32(Console.ReadLine());
             int results = skaitlis1 + skaitlis2;
@@ -22,7 +60,7 @@ namespace ConsoleApp1
 
 
         }
-        /*static void Funkcija()
+        static void Funkcija()
         {
             int a,b,c,d;
 
@@ -38,16 +76,18 @@ namespace ConsoleApp1
             Console.ReadLine();
 
 
-        }*/
+        }
+       
 
 
 
 
-          
-        
 
 
-    }
+
+
+
+}
 
 
     
